5cb1fdca6773df41e4f13084733c78cb7640104a
maxtorm <maxtorm12138@gmail.com>
2021-05-18 22:30:21 +0800

    ...

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
             output = '%04X    ' % base
             for offset in range(0, 16, 1):
                 output += '%02X ' % dump[base + offset]
-                if offset % 4 == 0:
+                if offset % 4 == 0 and offset != 0:
                     output += '    '
             print(output)
 
